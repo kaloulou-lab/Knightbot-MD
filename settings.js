6fb5371904df7a8ca427e58ccc813f31a80ee59a
kaloulou-lab <hosms088@gmail.com>
2025-03-31 22:33:36 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'kaloulou',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Kaloulou",
+  botOwner: 'shadow', // Your name
+  ownerNumber: '224662236019', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
